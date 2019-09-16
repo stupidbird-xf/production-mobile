f2aaab8ef59715e5dbe53fd9b426f9802cf96ad7
futianjiao <futianjiao@chengjuiot.com>
2019-09-16 15:48:27 +0800

    没有backlogin重新登录

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,88 +1,95 @@
 <template>
   <div class="home">
-    <van-row class="top-tip">
-      <van-col span="17">
-        <p>使用指南：<br/>1、在下方列表选择需要挂接的产品信息<br/>2、点击“挂接”按钮</p>
-      </van-col>
-      <van-col span="7">
-        <img class="product-code-img" src="@/components/img/product-code-img.png" alt="">
-      </van-col>
-    </van-row>
-    <div class="card">
-      <div class="title">码信息</div>
-      <div class="message">
-        <p>类型：{{ codeMsg.type }}</p>
-        <p>数量：{{ codeMsg.num }}</p>
-      </div>
-    </div>
-    <div class="card">
-      <div class="title">选择产品</div>
-      <van-dropdown-menu>
-        <van-dropdown-item
-          v-model="value"
-          :options="productOptions"
-          @change="changeValue"/>
-      </van-dropdown-menu>
-      <div
-        v-if="showList"
-        class="product-msg">
-        <van-row class="list-title border-outside">
-          <van-col class="border-inside" span="7">产品名称</van-col>
-          <van-col class="border-inside" span="6">英文ID</van-col>
-          <van-col class="border-inside" span="6">产品编码</van-col>
-          <van-col class="border-inside" span="5">积分奖励</van-col>
-        </van-row>
-        <van-row>
-          <van-col class="border-inside" span="7">{{ productMsg.proName }}</van-col>
-          <van-col class="border-inside" span="6">{{ productMsg.sname }}</van-col>
-          <van-col class="border-inside" span="6">{{ productMsg.signId }}</van-col>
-          <van-col class="border-inside" span="5">{{ productMsg.reward }}</van-col>
-        </van-row>
-      </div>
-    </div>
-    <van-button
-      class="confirm-btn"
-      type="default"
-      @click="bindProduct">确认挂接</van-button>
-    <van-popup v-model="show">
-      <div class="confirm-pop">
-        <img
-          class="close"
-          @click="closePop"
-          src="@/components/img/close.png">
-        <p class="title">产品码挂接成功</p>
-        <img
-          class="success"
-          src="@/components/img/success.png">
-        <div class="content">
-          <p><span class="name">产品名称：</span>{{ productMsg.proName }}</p>
-          <p><span class="name">英文ID：</span>{{ productMsg.sname }}</p>
-          <p><span class="name">产品编码：</span>{{ productMsg.productLicense }}</p>
-          <p><span class="name">积分：</span>{{ productMsg.reward }}</p>
-          <p><span class="name">码类型：</span>{{ productMsg.type }}</p>
-          <p><span class="name">数量：</span>{{ productMsg.proNum }}</p>
-        </div>
-        <!-- <van-button
-          class="confirm-btn"
-          type="default"
-          @click="confirmClick">确认</van-button> -->
-        <p class="code-tip">请扫描此码完成信息上链操作</p>
-        <div class="code-content">
-          <div class="qr-code" id="qrcode"></div>
+    <div v-if="showPage">
+      <van-row class="top-tip">
+        <van-col span="17">
+          <p>使用指南：<br/>1、在下方列表选择需要挂接的产品信息<br/>2、点击“挂接”按钮</p>
+        </van-col>
+        <van-col span="7">
+          <img class="product-code-img" src="@/components/img/product-code-img.png" alt="">
+        </van-col>
+      </van-row>
+      <div class="card">
+        <div class="title">码信息</div>
+        <div class="message">
+          <p>类型：{{ codeMsg.type }}</p>
+          <p>数量：{{ codeMsg.num }}</p>
         </div>
       </div>
-    </van-popup>
+      <div class="card">
+        <div class="title">选择产品</div>
+        <van-dropdown-menu>
+          <van-dropdown-item
+            v-model="value"
+            :options="productOptions"
+            @change="changeValue"/>
+        </van-dropdown-menu>
+        <div
+          v-if="showList"
+          class="product-msg">
+          <van-row class="list-title border-outside">
+            <van-col class="border-inside" span="7">产品名称</van-col>
+            <van-col class="border-inside" span="6">英文ID</van-col>
+            <van-col class="border-inside" span="6">产品编码</van-col>
+            <van-col class="border-inside" span="5">积分奖励</van-col>
+          </van-row>
+          <van-row>
+            <van-col class="border-inside" span="7">{{ productMsg.proName }}</van-col>
+            <van-col class="border-inside" span="6">{{ productMsg.sname }}</van-col>
+            <van-col class="border-inside" span="6">{{ productMsg.signId }}</van-col>
+            <van-col class="border-inside" span="5">{{ productMsg.reward }}</van-col>
+          </van-row>
+        </div>
+      </div>
+      <van-button
+        class="confirm-btn"
+        type="default"
+        @click="bindProduct">确认挂接</van-button>
+      <van-popup v-model="show">
+        <div class="confirm-pop">
+          <img
+            class="close"
+            @click="closePop"
+            src="@/components/img/close.png">
+          <p class="title">产品码挂接成功</p>
+          <img
+            class="success"
+            src="@/components/img/success.png">
+          <div class="content">
+            <p><span class="name">产品名称：</span>{{ productMsg.proName }}</p>
+            <p><span class="name">英文ID：</span>{{ productMsg.sname }}</p>
+            <p><span class="name">产品编码：</span>{{ productMsg.productLicense }}</p>
+            <p><span class="name">积分：</span>{{ productMsg.reward }}</p>
+            <p><span class="name">码类型：</span>{{ productMsg.type }}</p>
+            <p><span class="name">数量：</span>{{ productMsg.proNum }}</p>
+          </div>
+          <!-- <van-button
+            class="confirm-btn"
+            type="default"
+            @click="confirmClick">确认</van-button> -->
+          <p class="code-tip">请扫描此码完成信息上链操作</p>
+          <div class="code-content">
+            <div class="qr-code" id="qrcode"></div>
+          </div>
+        </div>
+      </van-popup>
+    </div>
+    <div v-else>
+      <div class="no-action">错误！请按规范操作！</div>
+    </div>
   </div>
 </template>
 
 <script>
 import QRCode from 'qrcodejs2';
+import Cookies from 'js-cookie';
 
 export default {
   name: 'home',
   data() {
     return {
       show: false,
+      showPage: false,
       codeMsg: {},
       productMsg: {},
       id: '',
@@ -101,35 +108,60 @@ export default {
     };
   },
   created() {
-    // 判断是否是微信端打开
-    const userNavigator = navigator.userAgent;
-    // eslint-disable-next-line
-    let timer = setInterval(() => {
-      if (!this.msgAllGet) {
-        this.id = this.getUrlKey('id');
-        this.backLogin = this.getUrlKey('backLogin');
-        this.address = this.getUrlKey('address');
-        if (this.backLogin !== '' && this.id !== '' && this.address !== '') {
-          this.msgAllGet = true;
-        }
+    console.log(!this.getUrlKey('backLogin'));
+    console.log(!this.getUrlKey('code'));
+    if (!this.getUrlKey('backLogin') && !this.getUrlKey('code')) {
+      window.location.href = `https://reitschain.com/code/login?redirect_url=${window.location.href}&connect_redirect=1`;
+    } else {
+      if (!this.getUrlKey('backLogin')) {
+        this.getBackLogin ();
       } else {
-        clearInterval(timer);
-        timer = null;
-        if (userNavigator.toLowerCase().match(/micromessenger/i) == 'micromessenger') {
-        this.msgAllGet = true;
-        this.getBatchById(this.id);
-        } else {
-          this.$notify({
-            title: '网络错误',
-            message: '请在微信端打开页面',
-            background: '#ffe1e1',
-            duration: 3000
-          });
-        }
+        this.view();
       }
-    }, 300);
+    }
   },
   methods: {
+    view() {
+      // 判断是否是微信端打开
+      const userNavigator = navigator.userAgent;
+      // eslint-disable-next-line
+      let timer = setInterval(() => {
+        if (!this.msgAllGet) {
+          this.id = this.getUrlKey('id');
+          this.backLogin = this.getUrlKey('backLogin') || Cookies.get('backLogin');
+          this.address = this.getUrlKey('address');
+          if (this.backLogin !== '' && this.id !== '' && this.address !== '') {
+            this.msgAllGet = true;
+            this.showPage = true;
+          }
+        } else {
+          clearInterval(timer);
+          timer = null;
+          if (userNavigator.toLowerCase().match(/micromessenger/i) == 'micromessenger') {
+          this.msgAllGet = true;
+          this.getBatchById(this.id);
+          } else {
+            this.$notify({
+              title: '网络错误',
+              message: '请在微信端打开页面',
+              background: '#ffe1e1',
+              duration: 3000
+            });
+          }
+        }
+      }, 300);
+    },
+    async getBackLogin () {
+      let result = await this.$http.get(`/backapi/admin/adminLogin?code=${this.getUrlKey('code')}`);
+      if (result._http_status !== 200 || result.code !== 0) {
+        this.$notify({
+          message: result.msg
+        });
+        return;
+      }
+      Cookies.set('backLogin', result.backLogin);
+      this.view();
+    },
     changeValue() {
       this.getMessage();
     },
@@ -385,6 +417,12 @@ export default {
         transform: translate(-50%, 0);
       }
     }
+  }
+  .no-action {
+    font-size: 36px;
+    padding-top: 300px;
+    color: #333;
+    text-align: center;
   }
 }
 </style>
